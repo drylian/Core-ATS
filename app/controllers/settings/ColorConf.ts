cf8aef460408cf67daed0cf44e9ce378f6c96a99
Drylian <109999325+drylian@users.noreply.github.com>
2023-10-29 03:30:56 -0300

    Atulizando cores, para cores mas "mesclaveis"

--- app/controllers/settings/ColorConf.ts
+++ app/controllers/settings/ColorConf.ts
@@ -23,12 +23,12 @@ export default async function ColorConf(core: LoggingsMethods) {
         const black = vars?.black; // prefix Black
         if (!black?.color?.primary) jsonsv(root.configPATH + '/color.json', { black: { color: { primary: '#000' } } });
         if (!black?.color?.secondary)
-            jsonsv(root.configPATH + '/color.json', { black: { color: { secondary: '#1d5285' } } });
+            jsonsv(root.configPATH + '/color.json', { black: { color: { secondary: '#1e1e1e' } } });
         if (!black?.color?.tertiary)
-            jsonsv(root.configPATH + '/color.json', { black: { color: { tertiary: '#808080' } } });
+            jsonsv(root.configPATH + '/color.json', { black: { color: { tertiary: '#0c0c0c' } } });
         if (!black?.text?.primary) jsonsv(root.configPATH + '/color.json', { black: { text: { primary: '#ffffff' } } });
         if (!black?.text?.secondary)
-            jsonsv(root.configPATH + '/color.json', { black: { text: { secondary: '#051322' } } });
+            jsonsv(root.configPATH + '/color.json', { black: { text: { secondary: '#d9d9d9' } } });
         if (!black?.text?.tertiary)
             jsonsv(root.configPATH + '/color.json', { black: { text: { tertiary: '#808080' } } });
         if (!black?.background) jsonsv(root.configPATH + '/color.json', { black: { background: '#000' } });
@@ -39,12 +39,12 @@ export default async function ColorConf(core: LoggingsMethods) {
         const white = vars?.white; // prefix White
         if (!white?.color?.primary) jsonsv(root.configPATH + '/color.json', { white: { color: { primary: '#fff' } } });
         if (!white?.color?.secondary)
-            jsonsv(root.configPATH + '/color.json', { white: { color: { secondary: '#1d5285' } } });
+            jsonsv(root.configPATH + '/color.json', { white: { color: { secondary: '#d5d5d5' } } });
         if (!white?.color?.tertiary)
-            jsonsv(root.configPATH + '/color.json', { white: { color: { tertiary: '#808080' } } });
-        if (!white?.text?.primary) jsonsv(root.configPATH + '/color.json', { white: { text: { primary: '#fff' } } });
+            jsonsv(root.configPATH + '/color.json', { white: { color: { tertiary: '#c1bcbc' } } });
+        if (!white?.text?.primary) jsonsv(root.configPATH + '/color.json', { white: { text: { primary: '#000' } } });
         if (!white?.text?.secondary)
-            jsonsv(root.configPATH + '/color.json', { white: { text: { secondary: '#1d5285' } } });
+            jsonsv(root.configPATH + '/color.json', { white: { text: { secondary: '#414040' } } });
         if (!white?.text?.tertiary)
             jsonsv(root.configPATH + '/color.json', { white: { text: { tertiary: '#808080' } } });
         if (!white?.background) jsonsv(root.configPATH + '/color.json', { white: { background: '#fff' } });
